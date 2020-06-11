84f20bbcf8c9a1d20cb8d4e25bba7ac871fa643f
ftelnov <fntelnovfn@gmail.com>
2020-06-11 16:36:56 +0300

    Залил основной функционал. Добавил пару текстовых полей в строковые ресурсы. По клику на поле с результатом можно скопировать значение поля (удобно же :))

--- app/src/main/java/com/example/androidtestschool/MainActivity.kt
+++ app/src/main/java/com/example/androidtestschool/MainActivity.kt
@@ -1,11 +1,38 @@
 package com.example.androidtestschool
 
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
+
+const val EXCHANGE_RATE = 74 // Курс обмена рубля на доллар
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // Скопировать значение из окна результата
+        resultAmount.setOnClickListener {
+            val clipboard =
+                getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+            clipboard.setPrimaryClip(ClipData.newPlainText("rate", resultAmount.text))
+        }
+        transferButton.setOnClickListener {
+            transferButton.hideKeyboard() // прячем клаву
+            resultAmount.visibility = TextView.GONE // прячем результирующее значение
+            val userInput = rubInputLayout.text.toString() // берем из поля данные
+            val finalFloat = userInput.toFloatOrNull() // приводим к флоату
+            if (finalFloat == null) {
+                // Если не получилось, пишем пользователю что неправильный формат ввода
+                this@MainActivity.showPrimaryToast(resources.getString(R.string.wrong_format_warning))
+            } else {
+                val result = finalFloat / 74 // получаем значение в долларах
+                resultAmount.text = result.toString() // приводим значение к строковому
+                resultAmount.visibility = TextView.VISIBLE // Показываем поле с результатом
+            }
+        }
     }
 }
--- app/src/main/java/com/example/androidtestschool/Utils.kt
+++ app/src/main/java/com/example/androidtestschool/Utils.kt
@@ -9,17 +9,20 @@ import android.widget.Toast
 import androidx.core.view.get
 import kotlinx.android.synthetic.main.primary_toast.view.*
 
+
+// Функция быстрого вызова тоста
 fun Context.showPrimaryToast(message: CharSequence) {
-    val toast = Toast.makeText(this, message, Toast.LENGTH_SHORT)
-    val toastContainer = LinearLayout(this)
+    val toast = Toast.makeText(this, message, Toast.LENGTH_SHORT) // Получаем тост через статик метод
+    val toastContainer = LinearLayout(this) // Получаем контейнер из контекста
     val inflater: LayoutInflater =
-        this.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
-    inflater.inflate(R.layout.primary_toast, toastContainer)
-    toastContainer[0].message.text = message
-    toast.view = toastContainer
-    toast.show()
+        this.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater // Получаем инфлэйтер через систему
+    inflater.inflate(R.layout.primary_toast, toastContainer) // Вставляем тост в лайаут контекста
+    toastContainer[0].message.text = message // ставим ему свой текст
+    toast.view = toastContainer // и устанавливаем вьюшкой тоста собранный лайаут
+    toast.show() // показываем тост
 }
 
+// Утилса для быстрого спрятывания клавиатуры, вызывается на вьюшке
 fun View.hideKeyboard() {
     val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
     imm.hideSoftInputFromWindow(windowToken, 0)
