1c5113d2661c2d6a126aa19d4c73cd5120df5d22
ftelnov <fntelnovfn@gmail.com>
2020-06-11 17:31:02 +0300

    Версия номер 2 готова (+ быстрый перевод значений)

--- app/src/main/java/com/example/androidtestschool/MainActivity.kt
+++ app/src/main/java/com/example/androidtestschool/MainActivity.kt
@@ -1,12 +1,12 @@
 package com.example.androidtestschool
 
-import android.content.ClipData
-import android.content.ClipboardManager
-import android.content.Context
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
+import android.text.Editable
+import android.text.InputType
+import android.text.TextWatcher
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+
 
 const val EXCHANGE_RATE = 74 // Курс обмена рубля на доллар
 
@@ -15,23 +15,49 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         exchangeRate.text = EXCHANGE_RATE.toString()
-        // Скопировать значение из окна результата
-        resultAmount.setOnClickListener {
-            copyToClipboard(resultAmount.text)
-        }
-        transferButton.setOnClickListener {
-            transferButton.hideKeyboard() // прячем клаву
-            resultAmount.visibility = TextView.GONE // прячем результирующее значение
-            val userInput = rubInputLayout.text.toString() // берем из поля данные
-            val finalFloat = userInput.toFloatOrNull() // приводим к флоату
-            if (finalFloat == null) {
-                // Если не получилось, пишем пользователю что неправильный формат ввода
-                this@MainActivity.showPrimaryToast(resources.getString(R.string.wrong_format_warning))
-            } else {
-                val result = finalFloat / 74 // получаем значение в долларах
-                resultAmount.text = result.toString() // приводим значение к строковому
-                resultAmount.visibility = TextView.VISIBLE // Показываем поле с результатом
+        rubInputLayout.inputType = InputType.TYPE_CLASS_NUMBER or InputType.TYPE_NUMBER_FLAG_DECIMAL
+        usdInputLayout.inputType = InputType.TYPE_CLASS_NUMBER or InputType.TYPE_NUMBER_FLAG_DECIMAL
+        var rubChangedListener: TextWatcher? = null
+        var usdChangedListener: TextWatcher? = null
+        rubChangedListener = object: TextWatcher {
+            override fun afterTextChanged(p0: Editable?) {
+                val rubInput = p0.toString().toFloatOrNull()
+                if (rubInput != null) {
+                    val usdInput = rubInput / EXCHANGE_RATE
+                    usdInputLayout.removeTextChangedListener(usdChangedListener)
+                    usdInputLayout.setText(usdInput.toString())
+                    usdInputLayout.addTextChangedListener(usdChangedListener)
+                }
             }
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+        }
+        usdChangedListener = object : TextWatcher {
+            override fun afterTextChanged(p0: Editable?) {
+                val usdInput = p0.toString().toFloatOrNull()
+                if (usdInput != null) {
+                    val rubInput = usdInput * EXCHANGE_RATE
+                    rubInputLayout.removeTextChangedListener(rubChangedListener)
+                    rubInputLayout.setText(rubInput.toString())
+                    rubInputLayout.addTextChangedListener(rubChangedListener)
+                }
+            }
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+        }
+        rubInputLayout.addTextChangedListener(rubChangedListener)
+        usdInputLayout.addTextChangedListener(usdChangedListener)
+        // Скопировать значение из окон результатов
+        copyResults.setOnClickListener {
+            copyToClipboard("Количество в рублях: ${rubInputLayout.text} | Количество в долларах: ${usdInputLayout.text}")
         }
     }
 }
